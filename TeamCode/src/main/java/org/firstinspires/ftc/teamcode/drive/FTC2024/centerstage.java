d3d50eafe2099b7dddd0e9a030b9d64925b2c6d9
alsdn8754 <jedward9321@gmail.com>
2024-01-08 10:42:20 +0900

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/centerstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/centerstage.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode.drive.FTC2024;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
