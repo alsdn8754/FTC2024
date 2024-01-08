531e9da1060dfc9f16aee82379a3536423823429
alsdn8754 <jedward9321@gmail.com>
2024-01-08 10:45:00 +0900

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/drivetest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/drivetest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 
-public class centerstage extends LinearOpMode {
+public class drivetest extends LinearOpMode {
 
     @Override
 
