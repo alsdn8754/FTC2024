620d49833edd33c8e8e4ca5e7669a109907cb4d9
alsdn8754 <jedward9321@gmail.com>
2024-01-15 10:53:54 +0900

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/TensorFlowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/TensorFlowTest.java
@@ -124,8 +124,8 @@ public class TensorFlowTest extends LinearOpMode {
                 // choose one of the following:
                 //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
                 //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
-                //.setModelFileName(TFOD_MODEL_FILE)
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                .setModelFileName(TFOD_MODEL_FILE)
 
                 // The following default settings are available to un-comment and edit as needed to
                 // set parameters for custom models.
