27619a989f72944d29da3e9a1fb1a3a25afa087e
alsdn8754 <jedward9321@gmail.com>
2024-01-15 11:39:52 +0900

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/TensorFlowTestRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FTC2024/TensorFlowTestRed.java
@@ -31,13 +31,11 @@ package org.firstinspires.ftc.teamcode.drive.FTC2024;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-
 import java.util.List;
 
 /*
