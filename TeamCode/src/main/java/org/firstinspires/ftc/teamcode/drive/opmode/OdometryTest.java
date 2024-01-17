95899e0cef03a1c2ccf83c1e9295723d981a1750
minwoo <jedward9321@gmail.com>
2024-01-17 14:13:06 +0900

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/OdometryTest.java
@@ -2,25 +2,76 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Config
 @Autonomous(group = "drive")
 public class OdometryTest extends LinearOpMode {
+
+    private void customSleep(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    DcMotor armMotor;
+    DcMotor grabMotor;
+    Servo leftHandServo;
+    Servo wristServo;
+    Servo rightHandServo;
+
+    public void aawAdjust(double ArmPower, int ArmTarget, double GrabPower, int GrabTarget, double WristTarget) {
+
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        grabMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        grabMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        armMotor.setTargetPosition(ArmTarget);
+        grabMotor.setTargetPosition(GrabTarget);
+        wristServo.setPosition(WristTarget);
+
+        armMotor.setPower(ArmPower);
+        grabMotor.setPower(GrabPower);
+
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        grabMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        telemetry.update();
+    }
+
+    public void gripAdjust(double Left_Target, double Right_Target) {
+        leftHandServo.setPosition(Left_Target);
+        rightHandServo.setPosition(Right_Target);
+
+        telemetry.update();
+    }
+
     @Override
     public void runOpMode() {
+
+        armMotor = hardwareMap.dcMotor.get("ARM");
+        grabMotor = hardwareMap.dcMotor.get("grab");
+        leftHandServo = hardwareMap.servo.get("leftHand");
+        wristServo = hardwareMap.servo.get("wrist");
+        rightHandServo = hardwareMap.servo.get("rightHand");
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         drive.setPoseEstimate(new Pose2d(28, -65, 0));
 
-
-        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d(27,-65))
-                .lineToLinearHeading(new Pose2d(28, -45, Math.toRadians(90)))
+        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d(27, -65))
+                .lineToLinearHeading(new Pose2d(28, -43, Math.toRadians(90)))
                 .build();
 
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
@@ -29,10 +80,16 @@ public class OdometryTest extends LinearOpMode {
 
         waitForStart();
 
-        if(isStopRequested()) return;
+        if (isStopRequested()) return;
 
+        leftHandServo.setPosition(0.15);
+        rightHandServo.setPosition(0.85);
         drive.followTrajectory(traj1);
+        aawAdjust(0, 0, 1, 600, 0.5);
+       customSleep(200);
+        gripAdjust(0.5, 0.85);
+        customSleep(100);
+        gripAdjust(0.15, 0.85);
         drive.followTrajectory(traj2);
-
     }
 }
