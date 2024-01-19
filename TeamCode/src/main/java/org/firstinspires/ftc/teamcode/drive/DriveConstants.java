5306183a514f3fcdd8bcd8781df7b44d93a4e5c7
sonsw0973 <sonsw0973@naver.com>
2024-01-20 01:17:14 +0900

    my home test :) - sonsw0973

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0166;  // /rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0162;  // /rpmToVelocity(MAX_RPM);
     public static double kA = 0.002;
     public static double kStatic = 0.08;
 
