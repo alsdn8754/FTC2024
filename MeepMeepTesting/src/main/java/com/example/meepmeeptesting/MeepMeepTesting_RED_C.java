1ce6d1befb0d86eb122d18fff20d1da049d217ca
minwoo <jedward9321@gmail.com>
2024-01-22 11:07:46 +0900

    :)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_RED_C.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_RED_C.java
@@ -9,16 +9,20 @@ public class MeepMeepTesting_RED_C {
 
 
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(30, 30, Math.toRadians(210.816), Math.toRadians(60), 15.24)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(11.5, -65, 0))
+                        drive.trajectorySequenceBuilder(new Pose2d(11.5, -65, 0)) //no
 
                                 .lineToLinearHeading(new Pose2d(11.5, -41, Math.toRadians(130)))
 
+                                .lineToLinearHeading(new Pose2d(43.5, -37, Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(37.1, -5.1, Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(-59.8, -11.1, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(37.1, -10.5, Math.toRadians(0)))
                                 .lineToLinearHeading(new Pose2d(43.5, -37, Math.toRadians(0)))
 
                                 .lineToLinearHeading(new Pose2d(43.5, -60, Math.toRadians(270)))
